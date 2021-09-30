50cbbb3f5b0cab86f8cab38693f546e37fa6689e
Joshua Wong <jwong545@hotmail.co.uk>
2021-09-30 23:05:09 +0100

    Initial commit

---
+++ DancingGoatMvc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DancingGoat.Tests")]
+[assembly: AssemblyDescription("Tests a sample ASP.NET MVC 5 application that demonstrates integration with Kentico.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0a45968e-4b61-4159-bd9b-177f307c7fed")]
+
+[assembly: AssemblyProduct("DancingGoat.Tests")]
+[assembly: AssemblyCopyright("© 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.7115.13953")]
+[assembly: AssemblyInformationalVersion("12.0.29")]
---
+++ DancingGoatMvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DancingGoat")]
+[assembly: AssemblyDescription("A sample ASP.NET MVC 5 application that demonstrates integration with Kentico.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1e9951cc-99e1-4651-b44b-5c727ee08b91")]
+
+[assembly: AssemblyProduct("DancingGoat")]
+[assembly: AssemblyCopyright("© 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.7115.13953")]
+[assembly: AssemblyInformationalVersion("12.0.29")]
+
+[assembly: InternalsVisibleTo("DancingGoat.Tests")]
